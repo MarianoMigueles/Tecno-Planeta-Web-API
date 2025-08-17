f1b662d6bc56475971af3ffd60653c3f121d5314
MarianoMigueles <164581983+MarianoMigueles@users.noreply.github.com>
2025-08-17 00:42:14 -0300

    chore: scaffold base classes in API project, BLL, Entities and DAL.

---
+++ Entities/Users/User.cs
@@ -0,0 +1,7 @@
+﻿namespace Entities.Users
+{
+    public class User
+    {
+
+    }
+}
---
+++ Tecno Planeta Web API/Controllers/RepairController.cs
@@ -0,0 +1,6 @@
+﻿namespace Tecno_Planeta_Web_API.Controllers
+{
+    public class RepairController
+    {
+    }
+}
